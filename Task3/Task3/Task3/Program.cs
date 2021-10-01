37bf8c452bb6e6d43cdf761cc56f4377757219b3
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2021-10-01 23:19:09 +0300

    Add files via upload

---
+++ Task3/Task3/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accounting accounting = new Accounting(@"./input.txt");
+            Console.WriteLine(accounting.ToString());
+            Console.WriteLine(accounting.PrintSingularFlat(3));
+            Console.WriteLine(accounting.GetOwnerWithBiggestDebt());
+            Console.WriteLine(accounting.GetFlatNumberWithoutUsingElectricity());
+        }
+    }
+}
