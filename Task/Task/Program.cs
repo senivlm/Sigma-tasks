4f1df79713eb5b94bef5e0241e7d9157ddbf5051
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2021-10-01 14:19:24 +0300

    Add files via upload

---
+++ Task/Task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of products: ");
+            int numberOfProducts = int.Parse(Console.ReadLine());
+            Product[] products = new Product[numberOfProducts];
+            if (numberOfProducts > 0)
+            {
+                Console.WriteLine("Enter name, price and weight of each product.");
+                for (int i = 0; i < products.Length; ++i)
+                {
+                    Console.WriteLine($"Product number {i + 1}:");
+                    products[i] = new Product(Console.ReadLine(), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+                }
+                Buy buy = new Buy(products);
+                buy.AddProduct();
+                Console.WriteLine(Check.Print(buy));
+            }
+        }
+    }
+}
