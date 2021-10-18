e18bac23c09be746ccab3efeb10632a539a399d2
senivlm <91743906+senivlm@users.noreply.github.com>
2021-10-18 23:59:18 +0300

    Update Program.cs

--- Task7/WordReplacement/WordReplacement/Program.cs
+++ Task7/WordReplacement/WordReplacement/Program.cs
@@ -3,7 +3,7 @@
 namespace WordReplacement
 {
     class Program
-    {
+    {Є неперехоплені винятки
         static void Main(string[] args)
         {
             WordReplacer wordReplacer = new WordReplacer();
