22a70b46fe24c9cd7a8e4c25b6bcd92987c78e2c
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2021-10-26 23:47:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage storage = new Storage();
+            storage.ReadFromFile(@"./input.txt");
+            storage.OnLogIncorrectInfoToFile += StorageEvents.LogIncorrectDataInFile;
+            storage.OnCorrectDataProduct += StorageEvents.CorrectProductData;
+            storage.OnLogExpiredProducts += StorageEvents.LogExpiredProducts;
+            storage.AddProduct();
+            storage.ExcludeProduct("Pork");
+            Console.WriteLine(storage.ToString());
+        }
+    }
+}
