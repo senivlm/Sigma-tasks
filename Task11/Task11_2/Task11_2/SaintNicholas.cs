16be6bd06b602fbfda4a4817bd8b770aaf5bddc2
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2021-11-09 01:02:46 +0300

    Add files via upload

---
+++ Task11/Task11_2/Task11_2/SaintNicholas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task11_2
+{
+    class SaintNicholas
+    {
+        public delegate void FormGiftForChild(ChildRequest childRequest, GiftBuilder giftBuilder);
+
+        public event FormGiftForChild OnFormGift;
+
+        private static SaintNicholas instance;
+
+        private GiftBuilder giftBuilder;
+
+        private SaintNicholas()
+        {
+            giftBuilder = null;
+        }
+
+        public static SaintNicholas GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SaintNicholas();
+            }
+            return instance;
+        }
+
+        public GiftBuilder GiftBuilder
+        {
+            get { return giftBuilder; }
+            set { SaintNicholas.GetInstance().giftBuilder = value; }
+        }
+
+        public Gift FormGift(ChildRequest childRequest)
+        {
+            SaintNicholas.GetInstance().giftBuilder.CreateNewGift();
+            SaintNicholas.GetInstance()?.OnFormGift.Invoke(childRequest, giftBuilder);
+            return giftBuilder.GetGift();
+        }
+
+        public Gift GetGift()
+        {
+            return SaintNicholas.GetInstance().giftBuilder.GetGift();
+        }
+    }
+}
