04a68b31b5879c1c29bdc502c0f0d4bcaecd5508
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2021-11-09 00:58:48 +0300

    Add files via upload

---
+++ Task11_2/Task11_2/BoyishGiftBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task11_2
+{
+    class BoyishGiftBuilder : GiftBuilder
+    {
+        public override void FormToy(string name)
+        {
+            gift.Toy = name;
+        }
+
+        public override void FormEdibleGift(string name)
+        {
+            gift.EdibleGift = name;
+        }
+
+        public override void FormWish(string wish)
+        {
+            gift.Wish = wish;
+        }
+    }
+}
---
+++ Task11_2/Task11_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task11_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildRequest childRequest = new ChildRequest("Vladyslav", Sex.Male, 37, 10);
+            SaintNicholas saintNicholas = SaintNicholas.GetInstance();
+            saintNicholas.GiftBuilder = new GirlishGiftBuilder();
+            saintNicholas.OnFormGift += GiftFormingOptions.SecondOption;
+            Gift[] gifts = new Gift[15];
+            for (int i = 0; i < gifts.Length; ++i)
+            {
+                gifts[i] = saintNicholas.FormGift(childRequest);
+            }
+            for (int i = 0; i < gifts.Length; ++i)
+            {
+                Console.WriteLine(gifts[i].ToString());
+            }
+        }
+    }
+}
