726c5b89a8da10ce69919604922a3885a054ee5d
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2021-11-09 01:00:09 +0300

    Add files via upload

---
+++ Task11/Task11_2/Task11_2/ChildRequest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task11_2
+{
+    enum Sex { Male, Female }
+    class ChildRequest
+    {
+        private string name;
+
+        private Sex sex;
+
+        private int goodDeeds;
+
+        private int badDeeds;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                try
+                {
+                    if (value == "" || value == null)
+                        throw new ArgumentException("Child request name is empty");
+                    name = value;
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public Sex Sex
+        {
+            get { return sex; }
+            set 
+            {
+                sex = value;
+            }
+        }
+
+        public int GoodDeeds
+        {
+            get { return goodDeeds; }
+            set
+            {
+                try
+                {
+                    if (value < 0)
+                        throw new ArgumentException("Incorrect number of good deeds");
+                    goodDeeds = value;
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public int BadDeeds
+        {
+            get { return badDeeds; }
+            set
+            {
+                try
+                {
+                    if (value < 0)
+                        throw new ArgumentException("Incorrect number of bad deeds");
+                    badDeeds = value;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public ChildRequest()
+        {
+            name = null;
+            sex = Sex.Male;
+            goodDeeds = 0;
+            badDeeds = 0;
+        }
+
+        public ChildRequest(string name, Sex sex, int goodDeeds, int badDeeds)
+        {
+            this.name = name;
+            this.sex = sex;
+            this.goodDeeds = goodDeeds;
+            this.badDeeds = badDeeds;
+        }
+    }
+}
---
+++ Task11/Task11_2/Task11_2/Gift.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task11_2
+{
+    class Gift
+    {
+        private string toy;
+
+        private string edibleGift;
+
+        private string wish;
+
+        public string Toy
+        {
+            get { return toy; }
+            set 
+            {
+                try
+                {
+                    if (value == "" || value == null)
+                        throw new ArgumentException("Empty input name of toy");
+                    toy = value;
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public string EdibleGift
+        {
+            get { return edibleGift; }
+            set
+            {
+                try
+                {
+                    if (value == "" || value == null)
+                        throw new ArgumentException("Empty input name of toy");
+                    edibleGift = value;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public string Wish
+        {
+            get { return wish; }
+            set
+            {
+                try
+                {
+                    if (value == "" || value == null)
+                        throw new ArgumentException("Empty input name of toy");
+                    wish = value;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public Gift()
+        {
+            toy = null;
+            edibleGift = null;
+            wish = null;
+        }
+
+        public Gift(string toy, string edibleGift, string wish)
+        {
+            this.toy = toy;
+            this.edibleGift = edibleGift;
+            this.wish = wish;
+        }
+
+        public override string ToString()
+        {
+            if (EdibleGift != null)
+            {
+                return $"Toy: {Toy}, edible gift: {EdibleGift}, wish: {Wish}";
+            }
+            else
+            {
+                return $"Toy: {Toy}, wish: {Wish}";
+            }
+        }
+    }
+}
